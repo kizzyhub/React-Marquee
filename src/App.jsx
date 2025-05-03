c3896c6c2b181a532fde425eef917d6030431379
kizzyhub <bellokazeem908@gmail.com>
2025-05-03 10:26:05 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import "./App.css";
+import Marquee from "react-fast-marquee";
+
+const App = () => {
+  return (
+    <div
+      className="w-full min-h-screen flex flex-col justify-center items-center
+    bg-[#181818] text-neutral-200"
+    >
+      <div className="w-[50%] flex flex-col">
+        <div className="flex space-y-2 flex-col text-center mb-14">
+          <span className="text-5xl font-bold">My Skills</span>
+          <span className="text-2xl">
+            These are the skills I have acquired over the years.
+          </span>
+        </div>
+        <Marquee pauseOnHover>
+          <div
+            className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4
+          font-bold text-2xl cursor-pointer text-orange-400"
+          >
+            HTML
+          </div>
+          <div
+            className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4
+          font-bold text-2xl cursor-pointer text-purple-800"
+          >
+            Css
+          </div>
+          <div
+            className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4
+          font-bold text-2xl cursor-pointer text-yellow-400"
+          >
+            JavaScript
+          </div>
+          <div
+            className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4
+          font-bold text-2xl cursor-pointer text-blue-400"
+          >
+            React
+          </div>
+          <div
+            className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4
+          font-bold text-2xl cursor-pointer text-pink-700"
+          >
+            Vite
+          </div>
+          <div
+            className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4
+          font-bold text-2xl cursor-pointer text-neutral-400"
+          >
+            Tailwindcss
+          </div>
+        </Marquee>
+        <Marquee autoFill pauseOnHover direction="right"> 
+        <div
+            className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4
+          font-bold text-2xl cursor-pointer text-green-400"
+          >
+            Nodejs
+          </div>
+          <div
+            className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4
+          font-bold text-2xl cursor-pointer text-blue-500"
+          >
+            Github
+          </div>
+          <div
+            className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4
+          font-bold text-2xl cursor-pointer text-neutral-400"
+          >
+            R programming 
+          </div>
+          <div
+            className="bg-[#202020] m-1 rounded-xl flex space-x-5 p-4
+          font-bold text-2xl cursor-pointer text-green-600"
+          >
+            Python
+          </div>
+
+        </Marquee>
+      </div>
+    </div>
+  );
+};
+
+export default App;
